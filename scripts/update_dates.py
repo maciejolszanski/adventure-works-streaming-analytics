f65b209b2a59ab6373c7e5e837beffe403aaf8f4
Maciej Olszański <olszanskimaciej@wp.pl>
2024-11-09 13:54:04 +0100

    refactor code

--- scripts/update_dates.py
+++ scripts/update_dates.py
@@ -12,7 +12,17 @@ class DatesUpdater:
         self.column_config: dict[str, list[str]] = self._get_datetime_columns()
         self.max_transaction_date: datetime.date = self._get_transaction_max_date()
 
-    def _get_tables(self):
+    def _execute_query(self, query: str) -> list:
+        """Executes a query and returns results, with logging."""
+        try:
+            result = self.sql_server.execute_query(query)
+            logger.debug(f"Executed query: {query}")
+            return result
+        except Exception as e:
+            logger.error(f"Error executing query: {query} - {e}")
+            raise
+
+    def _get_tables(self) ->list[str]:
         query = """
             SELECT
                 CONCAT(TABLE_CATALOG, '.', TABLE_SCHEMA, '.', TABLE_NAME) as table_full_name
@@ -20,14 +30,13 @@ class DatesUpdater:
             WHERE TABLE_TYPE = 'BASE TABLE'
                 AND TABLE_SCHEMA <> 'dbo'
             """
-        query_result = self.sql_server.execute_query(query)
+        query_result = self._execute_query(query)
         all_tables = [table[0] for table in query_result]
-
-        logger.info("Succesfully read all tables")
+        logger.info("Succesfully retrieved all tables")
 
         return all_tables
 
-    def _get_datetime_columns(self):
+    def _get_datetime_columns(self) -> dict[str, list[str]]:
         query = f"""
             SELECT
                 CONCAT(TABLE_CATALOG, '.', TABLE_SCHEMA, '.', TABLE_NAME) as table_full_name
@@ -37,23 +46,20 @@ class DatesUpdater:
             AND CONCAT(TABLE_CATALOG, '.', TABLE_SCHEMA, '.', TABLE_NAME) in {tuple(self.all_tables)}
             GROUP BY CONCAT(TABLE_CATALOG, '.', TABLE_SCHEMA, '.', TABLE_NAME)
         """
-        date_columns = self.sql_server.execute_query(query)
+        date_columns = self._execute_query(query)
         logger.info("Succesfully read datetimes columns")
 
         column_config = self._generate_column_config(date_columns)
-
         return column_config
 
+    @staticmethod
     def _generate_column_config(
-            self,
             table_columns: list[tuple[str, str]]
         ) -> dict[str, list[str]]:
         """Converts list of tuples into dict,
         splits second elem of the tuple by ','.
         """
-
         config = {table: cols.split(',') for table, cols in table_columns}
-        logger.info("Succesfully prepared table-columns config")
 
         return config
 
@@ -67,55 +73,60 @@ class DatesUpdater:
         return max_transaction_date
     
     def _get_col_max_date(self, table: str, column: str) -> datetime.date | None:
-
         query = f"""
             SELECT MAX({column}) 
             FROM {table}
         """
-        query_result = self.sql_server.execute_query(query)
+        query_result = self._execute_query(query)
         max_date = query_result[0][0]
 
         if isinstance(max_date, datetime.datetime):
-            max_date = max_date.date()
-        elif not isinstance(max_date, datetime.date):
-            logger.warning(
-                f"{table}.{column} max_date is of {type(max_date)}, not a date or datetime object!"
-            )
+            return max_date.date()
+        elif isinstance(max_date, datetime.date):
+            return max_date
+        else:
+            logger.warning(f"{table}.{column} max_date is of type {type(max_date)}, not a date or datetime.")
+            return None
 
-        return max_date
-
-    def _get_diff_days_from_now(self, date: datetime.date) -> int:
+    @staticmethod
+    def _get_diff_days_from_now(date: datetime.date) -> int:
         date_diff = datetime.date.today() - date
         
         return date_diff.days
 
     def update_dates(self):
+        """Main function to update dates based on transaction and current date."""
+        
+        logger.info("updating days based on max transaction date.")
         self._updates_dates_based_on_transaction_max_date()
+        logger.info("updating days if later than today")
         self._update_dates_if_later_than_today()
 
     def _updates_dates_based_on_transaction_max_date(self):
-        days_to_add = self._get_diff_days_from_now(self.max_transaction_date)
+        """Updates dates by adding the difference from the max transaction date to today."""
 
+        days_to_add = self._get_diff_days_from_now(self.max_transaction_date)
         for table, columns in self.column_config.items():
             self._add_days_to_columns(table, columns, days_to_add)
 
     def _update_dates_if_later_than_today(self):
+        """Adjusts dates for each table if a column's max date is later than today."""
+
         today_date = datetime.date.today()
-
         for table, columns in self.column_config.items():
-            max_table_date = today_date
-
-            for column in columns:
-                max_col_date = self._get_col_max_date(table, column)
-                if not max_col_date:
-                    continue
-                elif max_col_date > max_table_date:
-                    max_table_date = max_col_date
             
+            if not columns:
+                logger.error(f"No columns provided for table {table}")
+                continue
+
+            max_table_date = max(
+                (self._get_col_max_date(table, column) or today_date for column in columns),
+            )
+
             if max_table_date > today_date:
                 # days_to_add should be negative here
                 days_to_add = self._get_diff_days_from_now(max_table_date)
-                logger.debug(f"{table}.{column}, max_table_date: {max_table_date}, days_diff: {days_to_add}")
+                logger.debug(f"{table}, max_table_date: {max_table_date}, days_diff: {days_to_add}")
                 self._add_days_to_columns(table, columns, days_to_add)
 
     def _add_days_to_columns(
@@ -124,10 +135,9 @@ class DatesUpdater:
             columns: list[str],
             days_to_add: int
         ):
-         
-        logging.debug(f"table: {table}, column: {columns}")
-        dateadd_strings = []
+        """Updates each column in the specified table by adding a given number of days."""
 
+        dateadd_strings = []
         for column in columns:
             dateadd_strings.append(
                 f"{column} = DATEADD(DAY, {days_to_add}, {column})"
@@ -136,14 +146,12 @@ class DatesUpdater:
         dateadd_string = ', '.join(dateadd_strings)
 
         query = f"""UPDATE {table} SET {dateadd_string}"""
-        logging.debug(query)
-
-        result = self.sql_server.execute_query(query)
-        logging.debug(result)
+        logger.debug(f"Updating table with query: {query}")
+        self._execute_query(query)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
+    logging.basicConfig(level="INFO")
 
     sql_server = SQLServerConnector()
     dates_updater = DatesUpdater(sql_server)
